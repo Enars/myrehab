29c422e0ba4851c9f7ec68e0b437c14a62ce95a8
Enars <ejnar.sorensen@gmail.com>
2017-05-22 11:34:02 +0200

    css

--- index.php
+++ index.php
@@ -1,22 +1,64 @@
 <?php include("include/bootstrap.php"); ?>
 <div class="header">
+  <nav>
+    <a class="title" href="#home">(LOGO) MyRehab</a>
     <ul>
-  <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li><a href="#contact">Contact</a></li>
-  <li><a href="#about">About</a></li>
-</ul>
+      <li><a class="active" href="#home">Profile</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#logout">Logout (bild)</a></li>
+    </ul>
+  </nav>
 </div>
 <div class="content">
-    <div="left-col">
-        <div class="group">ssss</div>
+    <div class="left-col">
+        <div class="group">
+          <ul>
+            <li><a class="user one" href="#">(bild) Thomas Placeholder</a></li>
+            <li><a class="user one" href="#">(bild) Jonas Placeholder</a></li>
+            <li><a href="#">(bild) En Annan Placeholder</a></li>
+            <li><a href="#">(bild) Tredje Placeholder</a></li>
+          </ul>
+        </div>
     </div>
     <div class="middle-col">
-        <div class="main">ssss</div>
+        <div class="main">
+          <p>sdsdsd</p>
+        </div>
     </div>
     <div class="right-col">
-        <div class="training">ssss</div>
-        <div class="social">ssss</div>
+        <div class="training">
+          <table>
+            <tr>
+              <td>Stå på händer</td>
+              <td>Tisdagar</td>
+              <td>10:00</td>
+            </tr>
+            <tr>
+              <td>Nån annan träning</td>
+              <td>Fredagar</td>
+              <td>12:00</td>
+            </tr>
+          </table>
+        </div>
+        <div class="social">
+          <table>
+            <tr>
+              <td>Fika</td>
+              <td>Tisdag 3/3</td>
+              <td>10:00</td>
+            </tr>
+            <tr>
+              <td>Mindfulness</td>
+              <td>Fredag 21/3</td>
+              <td>13:00</td>
+            </tr>
+            <tr>
+              <td>Nåt mer typ</td>
+              <td>Torsdag 23/5</td>
+              <td>11:00</td>
+            </tr>
+          </table>
+        </div>
     </div>
 </div>
 <div class="footer"></div>
