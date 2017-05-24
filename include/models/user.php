7861cbabcdc3e516fe7c80758cfc880b53850b1f
Enars <ejnar.sorensen@gmail.com>
2017-05-24 09:57:42 +0200

    3

--- include/models/user.php
+++ include/models/user.php
@@ -1,8 +1,20 @@
 <?php
+//Redirect the user via header
 function redirect($url, $permanent = false) {
 	if($permanent) {
 		header('HTTP/1.1 301 Moved Permanently');
 	}
 	header('Location: '.$url);
 	exit();
+}
+function isLoggedIn() {
+	if (!isset($_SESSION['loggedin']) && !$_SESSION['loggedin'] == true) {
+		//redirect
+		header('Location: '.'login.php');
+		exit();
+	}
+	//If is sjukgymnast -> priviliges
+	else if ($_SESSION['UserRole'] == 1) {
+
+	}
 }
